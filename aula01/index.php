b76a4f2f3a95c7ea00f3d831b90bd92e10142923
pedrohiiq <pedrohiq14@gmail.com>
2018-11-05 22:07:27 -0200

    organizando dirs

---
+++ aula01/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>tiiitulo</title>
+	<meta charset="utf-8">
+</head>
+<body>
+<?php  ?>
+</body>
+</html>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Pagina inicial</h1>
+	<p>Esta é a pagina principal!!!</p>
+	<p>Este é um outro parágrafo</p>
+	<p>texto~<br>~quebrado em duas linhas!!!!</p>
+	texto fora de paragrafo
+	texto~<br>~quebrado e fora de paragrafo
+	<p><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">link pro youtube!!!</a></p>
+	tabs	nao      funcionam,    nem                espacos          ? wot oh nao,    somente  um  espaco    conta!
+	<a href="https://google.com"><p><img src="peeledbanana.jpg"></p></a>
+	<h1>titulo 1</h1>
+	<h2>titulo 2</h2>
+	<h3>titulo 3</h3>
+	<h4>titulo 4</h4>
+	<h5>titulo 5</h5>
+	<h6>titulo 6</h6>
+	
+	<?php 
+		$nome = "araragi";
+		$fnome = "arararagi-san";
+		echo "shikashi boku no namae wa <b>" . $nome . ", </b> da, e nao " . $fnome;
+	?>
+</body>
+<body>
+	<p>outro paragrafo em outro<%--<body></body>--%></p>
+</body>
+</html>
